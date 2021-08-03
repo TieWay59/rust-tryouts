7356f1524a96ecedda2e7c742a1d5f7bec288702
TieWay59 <tieway59@foxmail.com>
2021-08-03 20:19:56 +0800

    add all

---
+++ examples/rs_05_ownership/target/debug/deps/rs_05_ownership-4428cf92b2c2ae88.d
@@ -0,0 +1,5 @@
+/root/rust-tryouts/rs_05_ownership/target/debug/deps/rs_05_ownership-4428cf92b2c2ae88.rmeta: src/main.rs
+
+/root/rust-tryouts/rs_05_ownership/target/debug/deps/rs_05_ownership-4428cf92b2c2ae88.d: src/main.rs
+
+src/main.rs:
---
+++ examples/rs_05_ownership/target/rls/debug/deps/rs_05_ownership-17920e1a08237ada.d
@@ -0,0 +1,5 @@
+/root/rust-tryouts/rs_05_ownership/target/rls/debug/deps/rs_05_ownership-17920e1a08237ada.rmeta: src/main.rs
+
+/root/rust-tryouts/rs_05_ownership/target/rls/debug/deps/rs_05_ownership-17920e1a08237ada.d: src/main.rs
+
+src/main.rs:
